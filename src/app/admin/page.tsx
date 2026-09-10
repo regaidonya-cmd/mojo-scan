4bedba84af288c4c018311ceeb07d83d05f809bf
regaidonya-cmd <regai.donya@gmail.com>
2026-09-10 09:53:59 +0200

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import { createClient } from '@supabase/supabase-js'
 import { redirect } from 'next/navigation'
 import { AdminView } from '@/components/admin/AdminView'
